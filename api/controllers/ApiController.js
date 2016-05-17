be22b91d3684d3c0941feac78d0c6a6cfd43f0fc
Mauritz Zachrisson <mauritz.zachrisson@gmail.com>
2016-05-17 19:06:42 +0200

    Implemented basic RESTful API

--- api/controllers/ApiController.js
+++ api/controllers/ApiController.js
@@ -7,6 +7,7 @@
 /* globals Content */
 /* jshint esnext: true */
 "use strict";
+var markdown = require('markdown').markdown;
 
 var fs = require('fs');
 
@@ -16,6 +17,53 @@ module.exports = {
         actions: false,
         shortcuts: false,
         rest: false
+    },
+    
+    list: function (req, res) {
+        var language = req.params.language;
+        var type = req.params.type;
+
+        var fields = ['id', 'content_type', 'author', 'image', 'sticky', 'publishDate', 'facebookEvent',
+            'googleForm', 'eventStart', 'eventEnd', 'eventLocation'];
+        fields.push('title_' + language);
+        fields.push('content_' + language);
+
+        Content
+            .find({
+                where: {
+                    publishDate: { '<': new Date() }
+                },
+                orderBy: 'publishDate DESC'
+            })
+            .exec(function (err, content) {
+                return res.jsonx(content.map(function (item) {
+                    // Limit result to given properties
+                    var i = {};
+                    Object.keys(item).forEach(key => { if (fields.indexOf(key) !== -1) return i[key] = item[key] });
+                    i['content_' + language] = markdown.toHTML(item['content_' + language]);
+
+                    return i;
+                }));
+            });
+    },
+
+    item: function (req, res) {
+        var id = req.params.id;
+
+        var fields = ['id', 'content_type', 'author', 'image', 'sticky', 'publishDate', 'facebookEvent',
+            'googleForm', 'eventStart', 'eventEnd', 'eventLocation', 'title_sv', 'title_en', 'content_sv', 'content_en'];
+
+        Content.findOne(id).exec(function (err, item) {
+            // Limit result to given properties
+            var i = {};
+            Object.keys(item).forEach(key => { if (fields.indexOf(key) !== -1) return i[key] = item[key] });
+
+            // Convert Markdown to HTML
+            i['content_sv'] = markdown.toHTML(item['content_sv']);
+            i['content_en'] = markdown.toHTML(item['content_en']);
+
+            return res.jsonx(i);
+        });
     }
     
 };
